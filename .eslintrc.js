0e4c91a8f4f0a3a56c534c1b59b2f612d8c9e7e4
almonzer-fadl <valmonzer@proton.me>
2025-05-04 18:48:10 +0300

    chore: add ESLint configuration and ignore files for improved code quality

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'next/core-web-vitals'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unused-vars': 'warn',
+    'no-undef': 'error',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+} 
